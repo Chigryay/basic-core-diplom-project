481d7ed94a7366f05662695b6e5d4381f7099f26
Chigryay Aleksandr <Fademon2019@gmail.com>
2025-09-16 00:45:33 +0700

    project is done

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import service.MenuNavigator;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        MenuNavigator navigator = new MenuNavigator();
+        navigator.start();
+    }
+}
--- src/data/Contact.java
+++ src/data/Contact.java
@@ -3,11 +3,21 @@ package data;
 import java.util.Objects;
 
 public class Contact {
+
     private String fullName;
     private String position;
     private String birthday;
     private String phoneNumber;
     private String email;
+
+    public Contact(String fullName, String position, String birthday, String phoneNumber, String email) {
+        this.fullName = fullName;
+        this.position = position;
+        this.birthday = birthday;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+
+    }
 
     public String getFullName() {
         return fullName;
@@ -29,11 +39,27 @@ public class Contact {
         return email;
     }
 
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     @Override
     public boolean equals(Object object) {
         if (object == null || getClass() != object.getClass()) return false;
         Contact contact = (Contact) object;
-        return contact.getFullName().equals(fullName);
+        return Objects.equals(contact.getFullName().toUpperCase(), fullName.toLowerCase());
     }
 
     @Override
